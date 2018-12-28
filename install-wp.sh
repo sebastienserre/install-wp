67005d5687d53fb0c53d754883612319239200af
sebastienserre <sebastien@thivinfo.com>
2018-12-28 15:01:02 +0100

    rm confidential infos

--- install-wp.sh
+++ install-wp.sh
@@ -5,9 +5,22 @@ function bot {
   echo -e "${blue}${bold}(｡◕‿◕｡)${normal}  $1"
 }
 
-#  ==============================
-#  = The show is about to begin =
-#  ==============================
+# ============================================
+# = Ask some questions to make the fun going =
+# ============================================
+echo "Which kind of WordPress ? classic or multisite"
+read type
+if [ $type = "multisite" ]
+then
+echo "Would you prefer it on subdomain ? yes/no"
+read subdomain
+fi
+echo $type ": " $subdomain
+exit
+
+#  =======================
+#  = The show must go on =
+#  =======================
 
 # Welcome !
 bot "${blue}${bold}Bonjour ! Je suis le bot!${normal}"
@@ -17,13 +30,13 @@ bot "Je télécharge WordPress pour $i: ${cyan}$1${normal}"
 wp core download --locale=fr_FR
 
 bot "Je crée le fichier de config WordPress de $i: ${cyan}$1${normal}"
-wp core config --dbname="oceanwp50" --dbuser="oceanwp50" --dbprefix="oceanwp50_" --dbpass="4qhMe49ZDndd"
+wp core config --dbname="" --dbuser="" --dbprefix="" --dbpass=""
 
 #bot "Je paramètre Wordpress :"
-#wp core install --url="https://oceanwp50.eglises.xyz" --title="Mutualisation OceanWP 5.0" --admin_user="sebastienserre" --admin_password="1303Sarah$" --admin_email="sebastien.serre@5p2p.fr"
+#wp core install --url="" --title="Mutualisation OceanWP 5.0" --admin_user="sebastienserre" --admin_password="" --admin_email=""
 
 bot "et un multisite, c'est parti!"
-wp core multisite-install --url="https://oceanwp50.eglises.xyz" --title="Mutualisation OceanWP 5.0" --admin_user="sebastienserre" --admin_password="1303Sarah$" --admin_email="sebastien.serre@5p2p.fr" --subdomains
+wp core multisite-install --url="" --title="Mutualisation OceanWP 5.0" --admin_user="sebastienserre" --admin_password="" --admin_email="" --subdomains
 
 bot "J'install le thème sir!"
 wp theme install oceanwp --activate
